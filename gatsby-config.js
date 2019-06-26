3e167a2f850f7da9ea769457d22fd899fd7d4fd9
danielmonk <danielmonk@msn.com>
2019-06-26 22:36:19 +0100

    update GA tracking code

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,32 +27,10 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-    // In your gatsby-config.js
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-76115201-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: false,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Enables Google Optimize using your container Id
-        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
-        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Any additional create only fields (optional)
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "auto",
+        trackingId: "UA-76115201-3",
       },
     },
     `gatsby-plugin-sass`,
@@ -63,12 +41,6 @@ module.exports = {
           include: /\.inline\.svg$/
         }
       }
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-76115201-3",
-      },
     },
   ],
 }
